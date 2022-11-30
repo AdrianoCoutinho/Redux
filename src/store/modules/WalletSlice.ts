0cd7d3565b3f7bd73bbb364d0a8118a23692ac8b
AdrianoCoutinho <adriano.98@outlook.com>
2022-11-30 19:42:48 -0300

    30.11.22

---
+++ src/store/modules/WalletSlice.ts
@@ -0,0 +1,65 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { WalletType } from "../../types";
+
+const initialState: WalletType = {
+  balance: 0,
+  income: [],
+  outcome: [],
+};
+
+const walletSlice = createSlice({
+  name: "wallet",
+  initialState,
+  reducers: {
+    addIncome(state, action: PayloadAction<number>) {
+      state.income.push(action.payload);
+    },
+    addOutcome(state, action: PayloadAction<number>) {
+      state.outcome.push(action.payload);
+    },
+    updateBalance(state, action: PayloadAction<number>) {
+      state.balance = action.payload;
+    },
+    // addContact(state, action: PayloadAction<ContactType>) {
+    //   const index = state.items.findIndex(
+    //     (item) => item.phone === action.payload.phone
+    //   );
+
+    //   if (index === -1) {
+    //     state.items.push(action.payload);
+    //     state.status = "success";
+    //     return state;
+    //   }
+
+    //   state.status = "error";
+    //   return state;
+    // },
+    // deleteContact(state, action: PayloadAction<ContactType>) {
+    //   const index = state.items.findIndex(
+    //     (item) =>
+    //       item.name === action.payload.name &&
+    //       item.phone === action.payload.phone
+    //   );
+
+    //   if (index >= 0) {
+    //     state.items.splice(index, 1);
+    //     state.status = "success";
+    //   }
+
+    //   state.status = "error";
+    //   return state;
+    // },
+    // clearStatusContacts(state) {
+    //   state.status = "";
+    //   return state;
+    // },
+    clear() {
+      return initialState;
+    },
+  },
+});
+
+export const { addIncome, addOutcome, updateBalance, clear } =
+  walletSlice.actions;
+export default walletSlice.reducer;
